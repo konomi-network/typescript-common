1a7f9cf5f5a40f2b80e6545f1821dfeb4ddde1d7
Khanh Ha <khanhhaquang@gmail.com>
2021-11-10 15:49:13 +0700

    add registry and fix configure-references

--- configure-references.js
+++ configure-references.js
@@ -13,51 +13,35 @@ const config = JSON.parse(fs.readFileSync('tsconfig.json').toString());
 config.files = [];
 config.references = [];
 
-(async function() {
-    if (isCI) {
-        // dont run it on CI
-        return;
-    }
+(async function () {
+	if (isCI) {
+		// dont run it on CI
+		return;
+	}
 
-    const { stdout, stderr } = await exec('yarn workspaces info --json');
-
-    const lines = stdout.split('\n');
-    const depthTree = lines.slice(1, lines.length - 2).join('\n');
-    const workspaces = JSON.parse(depthTree);
-
-    for (const name in workspaces) {
-        const workspace = workspaces[name];
-        const location = path.resolve(process.cwd(), workspace.location);
-        const tsconfigPath = path.resolve(location, 'tsconfig.json');
-        if (fs.existsSync(tsconfigPath)) {
-            config.references.push({
-                path: workspace.location,
-            });
-            const workspaceConfig = JSON.parse(
-                fs.readFileSync(tsconfigPath).toString(),
-            );
-            workspaceConfig.compilerOptions.composite = true;
-            workspaceConfig.references = [];
-            for (const dependency of workspace.workspaceDependencies) {
-                const dependecyLocation = path.resolve(
-                    process.cwd(),
-                    workspaces[dependency].location,
-                );
-                if (
-                    fs.existsSync(
-                        path.resolve(dependecyLocation, 'tsconfig.json'),
-                    )
-                ) {
-                    workspaceConfig.references.push({
-                        path: path.relative(location, dependecyLocation),
-                    });
-                }
-            }
-            fs.writeFileSync(
-                tsconfigPath,
-                JSON.stringify(workspaceConfig, undefined, 4),
-            );
-        }
-    }
-    fs.writeFileSync('tsconfig.json', JSON.stringify(config, undefined, 4));
+	const { stdout } = await exec('yarn workspaces info');
+	const workspaces = JSON.parse(stdout);
+	for (const name in workspaces) {
+		const workspace = workspaces[name];
+		const location = path.resolve(process.cwd(), workspace.location);
+		const tsconfigPath = path.resolve(location, 'tsconfig.json');
+		if (fs.existsSync(tsconfigPath)) {
+			config.references.push({
+				path: workspace.location,
+			});
+			const workspaceConfig = JSON.parse(fs.readFileSync(tsconfigPath).toString());
+			workspaceConfig.compilerOptions.composite = true;
+			workspaceConfig.references = [];
+			for (const dependency of workspace.workspaceDependencies) {
+				const dependecyLocation = path.resolve(process.cwd(), workspaces[dependency].location);
+				if (fs.existsSync(path.resolve(dependecyLocation, 'tsconfig.json'))) {
+					workspaceConfig.references.push({
+						path: path.relative(location, dependecyLocation),
+					});
+				}
+			}
+			fs.writeFileSync(tsconfigPath, JSON.stringify(workspaceConfig, undefined, 4));
+		}
+	}
+	fs.writeFileSync('tsconfig.json', JSON.stringify(config, undefined, 4));
 })();
