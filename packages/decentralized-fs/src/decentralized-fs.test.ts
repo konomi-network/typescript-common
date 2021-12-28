85bcba14352203b5a5307b26f891e7c09f6e7046
Khanh Ha <khanhhaquang@gmail.com>
2021-12-29 00:50:04 +0700

    update test cases

--- packages/decentralized-fs/src/decentralized-fs.test.ts
+++ packages/decentralized-fs/src/decentralized-fs.test.ts
@@ -1,24 +1,40 @@
 import DecentralizedFileStorage from './decentralized-fs';
 
+// start ipfs daemon before running test
+// example at localhost:5001
+
 describe('decentralized-fs', () => {
-	const dfs = new DecentralizedFileStorage('http://localhost:5001/');
+	let cidResult = '';
+	const dfs = new DecentralizedFileStorage('http://localhost:5001');
 
 	it('is online', async () => {
 		expect(await dfs.isOnline).toEqual(true);
 	});
 
 	it('version test', async () => {
-		expect((await dfs.version()).version).toEqual('0.11.1');
+		expect((await dfs.version()).version).not.toBeNull();
 	});
 
-	it('find test', async () => {
-		const cid = 'Qmcb5MJyP3DDBXBFJ9wVqLu8V1AA7SSbSJ4kurHHjDDnw7';
-		expect(await dfs.find(cid)).toEqual('hello, dfs!');
-	});
-
-	it('save test', async () => {
+	it('save test with simple string', async () => {
 		const value = 'hello, dfs!';
 		const cid = await dfs.save(value);
-		expect(cid).toEqual('Qmcb5MJyP3DDBXBFJ9wVqLu8V1AA7SSbSJ4kurHHjDDnw7');
+		cidResult = cid;
+		expect(cid).toEqual(cidResult);
+	});
+
+	it('find test simple string', async () => {
+		expect(await dfs.find(cidResult)).toEqual('hello, dfs!');
+	});
+
+	it('save test with json string', async () => {
+		const value = '[{a: 1}]';
+		const cid = await dfs.save(value);
+		cidResult = cid;
+		expect(cid).toEqual(cidResult);
+	});
+
+	it('find test json string', async () => {
+		const result = await dfs.find(cidResult);
+		expect(result).toEqual('[{a: 1}]');
 	});
 });
--- packages/decentralized-fs/src/utils/uint8array.ts
+++
@@ -1,9 +0,0 @@
-const uint8toString = (input: number[]): string => {
-	let dataString = '';
-	for (const v of input) {
-		dataString += String.fromCharCode(v);
-	}
-	return dataString;
-};
-
-export { uint8toString };
