e44a1fecec17bccc99b70fca98c0e0167d4801fd
lispking <lispking@qq.com>
2022-02-13 22:29:25 +0800

    ! add liquidator account

--- packages/ocean-lending/src/tests/liquidation.ts
+++ packages/ocean-lending/src/tests/liquidation.ts
@@ -7,23 +7,37 @@ import { Comptroller } from '../comptroller';
 import { ensure, loadWalletFromEncyrptedJson, loadWalletFromPrivate, readJsonSync, readPassword } from '../utils';
 import { enterMarkets, borrow, repayBorrow } from './borrowUtils';
 
-async function liquidateBorrow(account: Account, oToken: OToken, token: ERC20Token, comptroller: Comptroller) {
+async function liquidateBorrow(liquidatorAccount: Account, liquidatorOToken: OToken, liquidatorToken: ERC20Token,
+    borrowerAccount: Account, borrowerOToken: OToken, borrowerToken: ERC20Token, borrowerComptroller: Comptroller) {
     console.log("==== liquidateBorrow ====");
-    const erc20Before = await token.balanceOf(account.address);
-    const oTokenBefore = await oToken.balanceOf(account.address);
-    console.log("erc20Before:", erc20Before, " oTokenBefore:", oTokenBefore);
 
-    const closeFactor = await comptroller.liquidationIncentive();
-    console.log("closeFactor: ", closeFactor);
+    {
+        const erc20Before = await liquidatorToken.balanceOf(liquidatorAccount.address);
+        const oTokenBefore = await liquidatorOToken.balanceOf(liquidatorAccount.address);
+        console.log("liquidator erc20Before:", erc20Before, " oTokenBefore:", oTokenBefore);
+    }
+
+    const erc20Before = await borrowerToken.balanceOf(borrowerAccount.address);
+    const oTokenBefore = await borrowerOToken.balanceOf(borrowerAccount.address);
+    console.log("borrower erc20Before:", erc20Before, " oTokenBefore:", oTokenBefore);
+
+    const closeFactor = await borrowerComptroller.liquidationIncentive();
+    console.log("borrower closeFactor: ", closeFactor);
 
     const underlyingToBorrow = 10;
     const underlyingDecimals = 18;
     const repayAmount = underlyingToBorrow * Math.pow(10, underlyingDecimals);
-    await oToken.liquidateBorrow(account.address, repayAmount, oToken.address, { confirmations: 3 });
+    await liquidatorOToken.liquidateBorrow(borrowerAccount.address, repayAmount, borrowerOToken.address, { confirmations: 3 });
 
-    const erc20After = await token.balanceOf(account.address);
-    const oTokenAfter = await oToken.balanceOf(account.address);
-    console.log("erc20After:", erc20After, " oTokenAfter:", oTokenAfter);
+    {
+        const erc20After = await liquidatorToken.balanceOf(liquidatorAccount.address);
+        const oTokenAfter = await liquidatorOToken.balanceOf(liquidatorAccount.address);
+        console.log("liquidator erc20After:", erc20After, " oTokenAfter:", oTokenAfter);
+    }
+
+    const erc20After = await borrowerToken.balanceOf(borrowerAccount.address);
+    const oTokenAfter = await borrowerOToken.balanceOf(borrowerAccount.address);
+    console.log("borrower erc20After:", erc20After, " oTokenAfter:", oTokenAfter);
 
     ensure(
         erc20Before == erc20After,
@@ -36,50 +50,70 @@ async function main() {
 
     const web3 = new Web3(new Web3.providers.HttpProvider(config.nodeUrl));
 
-    let account: Account;
+    let liquidatorAccount: Account;
+    let borrowerAccount: Account;
     if (config.encryptedAccountJson) {
         const pw = await readPassword();
-        account = loadWalletFromEncyrptedJson(config.encryptedAccountJson, pw, web3);
+        liquidatorAccount = loadWalletFromEncyrptedJson(config.encryptedLiquidatorAccountJson, pw, web3);
+        borrowerAccount = loadWalletFromEncyrptedJson(config.encryptedAccountJson, pw, web3);
     } else if (config.privateKey) {
-        account = loadWalletFromPrivate(config.privateKey, web3);
+        liquidatorAccount = loadWalletFromPrivate(config.liquidatorPrivateKey, web3);
+        borrowerAccount = loadWalletFromPrivate(config.privateKey, web3);
     } else {
         throw Error("Cannot setup account");
     }
 
-    console.log("Using account:", account.address);
+    console.log("Using liquidator account:", liquidatorAccount.address);
+    console.log("Using borrower account:", borrowerAccount.address);
 
     // load the oToken object
     const oTokenAbi = readJsonSync('./config/oToken.json');
-    const oToken = new OToken(
+    const borrowerOToken = new OToken(
         web3,
         oTokenAbi,
         config.oTokens.oKono.address,
-        account,
+        borrowerAccount,
         config.oTokens.oKono.parameters
     );
 
     // load the erc20 token object
     const erc20Abi = readJsonSync('./config/erc20.json');
-    const erc20Token = new ERC20Token(
+    const borrowerErc20Token = new ERC20Token(
         web3,
         erc20Abi,
-        oToken.parameters.underlying,
-        account
+        borrowerOToken.parameters.underlying,
+        borrowerAccount
     );
 
     const comptrollerAbi = readJsonSync('./config/comptroller.json');
-    const comptroller = new Comptroller(
+    const borrowerComptroller = new Comptroller(
         web3,
         comptrollerAbi,
-        oToken.parameters.comptroller,
-        account
+        borrowerOToken.parameters.comptroller,
+        borrowerAccount
+    );
+
+    const liquidatorOToken = new OToken(
+        web3,
+        oTokenAbi,
+        config.oTokens.oKono.address,
+        liquidatorAccount,
+        config.oTokens.oKono.parameters
+    );
+
+    const liquidatorErc20Token = new ERC20Token(
+        web3,
+        erc20Abi,
+        liquidatorOToken.parameters.underlying,
+        liquidatorAccount
     );
 
     // actual tests
-    await enterMarkets(account, config.oTokens.oKono.address, comptroller);
-    await borrow(account, oToken, erc20Token);
-    await liquidateBorrow(account, oToken, erc20Token, comptroller);
-    await repayBorrow(account, oToken, erc20Token);
+    // await enterMarkets(borrowerAccount, config.oTokens.oKono.address, borrowerComptroller);
+    // await borrow(borrowerAccount, borrowerOToken, borrowerErc20Token);
+    await liquidateBorrow(liquidatorAccount, liquidatorOToken, liquidatorErc20Token,
+        borrowerAccount, borrowerOToken, borrowerErc20Token, borrowerComptroller);
+    // await repayBorrow(borrowerAccount, borrowerOToken, borrowerErc20Token);
 }
 
 main().then(() => exit(0)).catch((e) => { console.log(e); exit(1); });
