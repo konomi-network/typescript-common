66e3f05ebc1df489cc88827f180c450ebd12d1b1
kun-dao <kun-dao@outlook.com>
2022-02-25 10:42:21 +0800

    reimplement ocean-lending cli

---
+++ packages/konomi-CLI/README.md
@@ -0,0 +1,63 @@
+
+
+## OceanLending
+
+### Help
+```
+npx ts-node src/konomi.ts --help
+
+npx ts-node src/konomi.ts ocean --help
+
+npx ts-node src/konomi.ts ocean enterMarkets --help
+
+```
+
+### EnterMarkets
+```
+npx ts-node src/konomi.ts ocean enterMarkets  --config-path your-path-to-configuration-json   
+
+```
+
+*Abbreviation*
+```
+npx ts-node src/konomi.ts ocean  enterMarkets  -c your-path-to-configuration-json   
+```
+
+### depositWorks
+```
+npx ts-node src/konomi.ts  ocean deposit  --config-path your-path-to-configuration-json  --amount 1000 
+```
+*Abbreviation*
+```
+npx ts-node src/konomi.ts ocean deposit  -c your-path-to-configuration-json  -a 1000 
+```
+
+### redeemNoBorrow
+```
+npx ts-node src/konomi.ts ocean  redeem  --config-path your-path-to-configuration-json   
+```
+*Abbreviation*
+```
+npx ts-node src/konomi.ts ocean redeem  -c your-path-to-configuration-json
+```
+
+### Borrow
+```
+npx ts-node src/konomi.ts  ocean  borrow  --config-path your-path-to-configuration-json --amount 10
+```
+
+
+*Abbreviation*
+```
+npx ts-node src/konomi.ts ocean borrow -c your-path-to-configuration-json -a 10
+```
+
+### Repay
+```
+npx ts-node src/konomi.ts ocean repay --config-path your-path-to-configuration-json
+```
+*Abbreviation*
+```
+npx ts-node src/konomi.ts ocean repay  -c your-path-to-configuration-json
+```
+
--- packages/konomi-CLI/src/konomi.ts
+++ packages/konomi-CLI/src/konomi.ts
@@ -1,38 +1,84 @@
 import { exit } from 'process';
 import { Command, Option, OptionValues } from 'commander';
-import { OceanLendingHandler } from './CommandHandler';
+import { parseOceanLeningConfiguration } from './utils';
+import { depositWorks, redeemNoBorrow } from '../../ocean-lending/integration-tests/deposit';
+import { borrow, enterMarkets, repayBorrow } from '../../ocean-lending/integration-tests/borrow';
+import logger from './logger';
 
-async function main() {
-  const program: Command = new Command();
-  program
-    .option('-d, --debug', 'output extra debugging')
-    .requiredOption('-p, --config-path <path>', 'config path')
-    .addOption(new Option('-c, --cmd <command>', 'konomi client command').choices(['ocean']))
-    .addOption(new Option('-s, --sub-cmd <sub-command>', 'konomi client sub-command').choices(['enterMarkets','depositWorks','redeemNoBorrow', 'borrow', 'repay']))
-    .option('-t, --token <address>', 'token address', 'defualt address')
-    .option('-a, --amount <token>', 'The amount of tokens to use for operations');
+function makeOceanCommand(): Command {
+  const ocean = new Command('ocean');
+  ocean.description('execute ocean-lending command')
+  // Add enterMarkets subcommad
+  ocean
+    .command('enterMarkets')
+    .description('enter Markets and check the balance')
+    .requiredOption('-c, --config-path <configPath>', 'config path')
+    .action(async (options: OptionValues) => {
+      logger.info('OceanLending: enterMarkets configPath: %o', options.configPath);
+      const [account, oToken, token, comptroller, config, priceOracle] = await parseOceanLeningConfiguration(options.configPath);
+      const markets = [config.oTokens.oKono.address];
+      await enterMarkets(account, markets, comptroller);
+      logger.info('OceanLending: enterMarkets sucess!');
+    });
 
-  program.parse(process.argv);
-  const options: OptionValues = program.opts();
-  if (options.debug) console.log(options);
+  // Add deposit subcommad
+  ocean
+    .command('deposit')
+    .description('charge otoken deposit with ERC20Token')
+    .requiredOption('-c, --config-path <path>', 'config path')
+    .requiredOption('-a, --amount <amount>', 'The amount of tokens to use for operations')
+    .action(async (options: OptionValues) => {
+      logger.info('OceanLending: enterMarkets configPath: %o amount: %o ', options.configPath, options.amount);
+      const [account, oToken, token, comptroller, config, priceOracle] = await parseOceanLeningConfiguration(options.configPath);
+      await depositWorks(account, oToken, token, options.amount)
+      logger.info('OceanLending: deposit sucess!');
+    });
 
-  switch (options.cmd) {
-      case 'ocean':
-          console.log("oceanlending command started.");
-          const oceanLendingHandler = new OceanLendingHandler(options.configPath,options.subCmd, options.tokenAddress, options.amount)
-          await oceanLendingHandler.handle();
-          console.log("oceanlending command finished.");
-        break;
+  // Add redeem subcommad
+  ocean
+    .command('redeem')
+    .description('redeem all minted oToken')
+    .requiredOption('-c, --config-path <path>', 'config path')
+    .action(async (options: OptionValues) => {
+      logger.info('OceanLending: enterMarkets configPath: %o', options.configPath);
+      const [account, oToken, token, comptroller, config, priceOracle] = await parseOceanLeningConfiguration(options.configPath);
+      await redeemNoBorrow(account, oToken, token)
+      logger.info('OceanLending: redeem sucess!');
+    });
 
-      default:
-          console.log("unknown command.")
-          break;
-  }
+  // Add borrow subcommad
+  ocean
+    .command('borrow')
+    .description('brrow otoken use KONO as collateral')
+    .requiredOption('-c, --config-path <path>', 'config path')
+    .requiredOption('-a, --amount <amount>', 'The amount of tokens to use for operations')
+    .action(async (options: OptionValues) => {
+      logger.info('OceanLending: enterMarkets configPath: %o amount: %o ', options.configPath, options.amount);
+      const [account, oToken, token, comptroller, config, priceOracle] = await parseOceanLeningConfiguration(options.configPath);
+      await borrow(account, oToken, token, priceOracle, comptroller, options.amount);
+      logger.info('OceanLending: borrow sucess!');  
+    });
+
+  // Add repay subcommad
+  ocean
+    .command('repay')
+    .description('repay borrow balance')
+    .requiredOption('-c, --config-path <path>', 'config path')
+    .requiredOption('-a, --amount <amount>', 'The amount of tokens to use for operations')
+    .action(async (options: OptionValues) => {
+      logger.info('OceanLending: enterMarkets configPath: %o amount: %o ', options.configPath, options.amount);
+      const [account, oToken, token, comptroller, config, priceOracle] = await parseOceanLeningConfiguration(options.configPath);
+      await repayBorrow(account, oToken, token, priceOracle);
+      logger.info('OceanLending: repay sucess!');
+    });
+  return ocean;
 }
 
-main()
-	.then(() => exit(0))
-	.catch((e) => {
-		console.log(e);
-		exit(1);
-	});
+async function main() {
+  const konomi: Command = new Command('konomi');
+  // Add nested ocean commands.
+  konomi.addCommand(makeOceanCommand());
+  konomi.parse(process.argv);
+}
+
+main();
