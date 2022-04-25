6b28d3dce28fd31fbe78e583a1043a56ab365d87
willesxm <willeslau@gmail.com>
2022-04-25 09:17:50 +0000

    fix oracle decimals

--- packages/ocean-lending/src/clients/priceOracle.ts
+++ packages/ocean-lending/src/clients/priceOracle.ts
@@ -2,7 +2,7 @@ import Web3 from 'web3';
 import { Client } from './client';
 
 class PriceOracleAdaptor extends Client {
-  public static readonly DEFAULT_DECIMALS = 8;
+  public static readonly DEFAULT_DECIMALS = 18;
 
   public async getUnderlyingPrice(tokenAddress: string): Promise<string> {
     const price = await this.contract.methods.getUnderlyingPrice(tokenAddress).call();
