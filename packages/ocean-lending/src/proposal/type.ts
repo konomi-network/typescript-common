8645d767d72a454b05cfa27d047bafd7b58a0019
willesxm <willeslau@gmail.com>
2022-04-27 15:03:49 +0800

    add function access

--- packages/ocean-lending/src/proposal/type.ts
+++ packages/ocean-lending/src/proposal/type.ts
@@ -25,7 +25,7 @@ export abstract class BasePropsalDetails implements ProposalDetails {
     abstract abi(): any;
     abstract calldata(web3: Web3): String;
 
-    methodSelector(web3: Web3): String {
+    public methodSelector(web3: Web3): String {
         const abi = this.abi();
         const methodSignature = `${abi.name}(${abi.inputs.map((i: { type: any }) => i.type)})`;
         return web3.utils.keccak256(methodSignature).substr(0, 10);
